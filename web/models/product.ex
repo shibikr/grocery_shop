6f7b7d7dfeedb962551b2390860e0d087c4f22bd
shibi <shibidewdrops7@gmail.com>
2017-07-19 11:18:59 +0530

    Prasun/Shibi - add validations for product

--- web/models/product.ex
+++ web/models/product.ex
@@ -19,10 +19,11 @@ defmodule Groceryshop.Product do
     struct
     |> cast(params, @required_fields ++ @optional_fields)
     |> validate_required(Enum.map(@required_fields, &String.to_atom(&1)))
-#    |> validate_number(:price, greater_than: 0)
-    |> validate_length(:name, min: 0, max: 10)
-#    |> validate_length(:code, greater_than: 9)
-#    |> unique_constraints(:code)
+    |> validate_number(:price, greater_than_or_equal_to: 0.00)
+    |> validate_number(:price, less_than_or_equal_to: 9999999.99)
+    |> validate_length(:name, min: 1)
+    |> validate_length(:code, min: 10)
+    |> unique_constraint(:code)
   end
 
 end
