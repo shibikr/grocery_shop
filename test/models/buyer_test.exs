ee0c931b69530faa3afc02449b165cb54f6bef04
shibi <shibidewdrops7@gmail.com>
2017-07-19 12:12:52 +0530

    Prasun/Shibi - add test for model buyer

--- test/models/buyer_test.exs
+++ test/models/buyer_test.exs
@@ -5,6 +5,7 @@ defmodule Groceryshop.BuyerTest do
 
   @valid_attrs %{email: "abc@xyz.org", name: "John doe"}
   @invalid_attrs %{email: "abcattheratexyz.org", name: "John doe"}
+  @invalid_name %{email: "abc@xyz.org", name: ""}
 
   test "changeset with valid attributes" do
     changeset = Buyer.changeset(%Buyer{}, @valid_attrs)
@@ -15,4 +16,10 @@ defmodule Groceryshop.BuyerTest do
     changeset = Buyer.changeset(%Buyer{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "changeset with empty name" do
+    changeset = Buyer.changeset(%Buyer{}, @invalid_name)
+    refute changeset.valid?
+  end
+
 end
