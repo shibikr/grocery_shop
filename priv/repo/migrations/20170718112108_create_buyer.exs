f10215a7637d5b7c1d768983f8f3c052a7694e36
Sooraj Parameswaran <soorajp@thoughtworks.com>
2017-07-19 11:37:49 +0530

    [Sooraj] Adding db level validations for buyers

--- priv/repo/migrations/20170718112108_create_buyer.exs
+++ priv/repo/migrations/20170718112108_create_buyer.exs
@@ -3,11 +3,15 @@ defmodule Groceryshop.Repo.Migrations.CreateBuyer do
 
   def change do
     create table(:buyers) do
-      add :name, :string
-      add :email, :string
+      add :name, :string, null: false
+      add :email, :string, null: false
 
       timestamps()
     end
+
+    create unique_index(:buyers, ["lower(email)"])
+    create constraint(:buyers, "email_should_be_valid", check: "email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'")
+    create constraint(:buyers, "name_cannot_be_empty", check: "char_length(name) >= 1")
 
   end
 end
